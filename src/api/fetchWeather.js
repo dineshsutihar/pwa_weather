3802f1a5590db9ba4974fdaebfd8f1a2bd70bcc1
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-10-31 20:50:43 +0530

    added some todos to complete

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
+// Don't forget to replace the API_KEY and dont rely on this because it can be changed anytime.
 const API_KEY = 'f33a484cf794d08d0148764789aaba32';
 
 export const fetchWeather = async (query) => {
