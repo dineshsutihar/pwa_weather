3e18fd3e5225f98da6a4af226605cf064ce0a4f4
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-12-31 16:55:42 +0530

    refactor: refactored some code

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ import './App.css';
 const App = () => {
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState({});
-    
+
     const search = async (e) => {
-        if(e.key === 'Enter') {
+        if (e.key === 'Enter') {
             const data = await fetchWeather(query);
 
             setWeather(data);
+            console.log(data);
             setQuery('');
         }
     }
 
     return (
         <div className="main-container">
-            <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
+            <input type="text" className="search" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
             {weather.main && (
                 <div className="city">
                     <h2 className="city-name">
